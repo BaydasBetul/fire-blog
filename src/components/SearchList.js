b7ec2f0b375e70f4c05ab8e68726fec2f56b4510
BaydasBetul <73883555+BaydasBetul@users.noreply.github.com>
2022-03-21 01:53:24 +0100

    erst

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -1,16 +1,1 @@
-// import React from "react";
-// import CardComp from "./BlogCard";
-// import { Grid } from "@mui/material";
 
-// const SearchList = ({ filteredList }) => {
-//   const filtered = filteredList?.map((item) => (
-//     <CardComp key={item.id} item={item} />
-//   ));
-//   return (
-//     <Grid container style={{justifyItems:"center", marginTop: 20 }}>
-//       {filtered}
-//     </Grid>
-//   );
-// };
-
-// export default SearchList;
